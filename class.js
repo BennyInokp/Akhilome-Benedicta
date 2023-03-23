a6ef5cc9ba0bb714a8911793f8fc19b4aef8f52f
BennyInokp <benedictaakhilome@email.com>
2023-03-23 12:06:21 +0100

    first commit

---
+++ class.js
@@ -0,0 +1,14 @@
+function isprime(num) {
+  if (num === 1) {
+    return false;
+  } else if (num === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < num; x++) {
+      if (num % x === 0) {
+        return false, true;
+      }
+    }
+  }
+}
+console.log(isprime(7));
